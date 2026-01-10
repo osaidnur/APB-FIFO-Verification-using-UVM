9294e4c460fda198fe98fbf251111871b332213a
osaidnur <1210733@student.birzeit.edu>
2026-01-10 21:47:03 +0200

    remove back to back sequences and tests

--- eda_version/apb_tests.sv
+++ eda_version/apb_tests.sv
@@ -399,35 +399,3 @@ endclass : stress_test
 // #############################################################################################
 // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 // #############################################################################################
-
-// ==============================================================================
-// Back to Back Test - Rapid transaction test
-// ==============================================================================
-class back_to_back_test extends apb_fifo_base_test;
-  
-  `uvm_component_utils(back_to_back_test)
-  
-  function new(string name = "back_to_back_test", uvm_component parent = null);
-    super.new(name, parent);
-  endfunction : new
-  
-  task run_phase(uvm_phase phase);
-    fifo_reset_sequence reset_seq;
-    back_to_back_sequence seq;
-    
-    phase.raise_objection(this);
-    
-    reset_seq = fifo_reset_sequence::type_id::create("reset_seq");
-    reset_seq.start(env.agent.sequencer);
-    
-    seq = back_to_back_sequence::type_id::create("seq");
-    seq.start(env.agent.sequencer);
-    
-    phase.drop_objection(this);
-  endtask : run_phase
-  
-endclass : back_to_back_test
-
-// #############################################################################################
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-// #############################################################################################
